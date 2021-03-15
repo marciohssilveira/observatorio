d8db34327f0fb29a1c831672566af779a66eb79d
Marcio Silveira <marciohssilveira@gmail.com>
2021-03-15 17:20:36 -0300

    finished

--- observatorio.py
+++ observatorio.py
@@ -28,6 +28,10 @@ class PdfExtractor:
         pdf.close()
         return text
 
+    def get_title_from_news(self):
+        titles = [x.replace('\n', '').replace('  ', ' ') for x in re.split(r'\d-', re.findall(r'\d- (.*?)\n \n', self.get_text_from_pdf(), re.DOTALL)[0])]
+        return titles
+
     def get_period_from_text(self):
         """
         Utilizes a regex sentence to find a specific part of the text containing the interval of dates covered for
@@ -60,13 +64,24 @@ class PdfExtractor:
         return references
 
 
-path = './files'
-
-observatorio = {}
-for filename in glob.glob(os.path.join(path, '*.pdf')):  # Navigates through all the files in a specified folder
-    pdf = PdfExtractor(filename)
-    periods = pdf.get_period_from_text()
-    news = pdf.get_news_from_text()
-    references = pdf.get_reference_from_news()
-    observatorio[periods] = dict(zip(references, news))  # creates a dictionary (periods are the keys) of dictionaries
-    # containing the references as keys, the news as values
+if __name__ == '__main__':
+    filepath = './Observa Brasil'
+    for path, subdirs, files in os.walk(filepath):
+        for name in files:
+            file = os.path.join(path, name)  # Navigates through all the files in a specified folder
+            try:
+                pdf = PdfExtractor(file)
+                periods = pdf.get_period_from_text() # a string
+                titles = pdf.get_title_from_news() # a list of the titles
+                news = pdf.get_news_from_text() # a list of news
+                references = pdf.get_reference_from_news() # a list of references
+                for i, j, k in zip(news, references, titles):
+                    new = i.replace('\n', ' ').replace(';', ',')
+                    reference = j.replace('\n', ' ').replace(';', ',')
+                    title = k.replace('\n', ' ').replace(';', ',')
+                    line = f'{periods}; {title}; {reference}; {new}\n'
+                    with open('export.csv', 'a') as file:
+                        file.write(line)
+            except:
+                with open('errors.txt', 'a') as error_file:
+                    error_file.write(f'Erro no arquivo: {file}\n')
